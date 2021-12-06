61d349651746367a9077fa4dba78f6e1a36ceea4
Vitaliy Novak <almuert@yandex.ru>
2021-12-06 07:00:30 +0500

    Fix naming

---
+++ http/bitrix24/adapter.go
@@ -0,0 +1,3 @@
+package birtix24
+
+
---
+++ http/bitrix24/scheme.go
@@ -0,0 +1,7 @@
+package birtix24
+
+type Contact struct {
+	Name       string `json:"NAME"`
+	SecondName string `json:"SECOND_NAME"`
+	LastName   string `json:"LAST_NAME"`
+}
