0ed479bf4ba03c1c71898116b79a3333b5c40984
vitaluy novak <demo3@optic.ru>
2021-12-07 23:49:47 +0500

    Delete test files

--- http/bitrix24/adapter.go
+++
@@ -1,3 +0,0 @@
-package birtix24
-
-
--- http/bitrix24/scheme.go
+++
@@ -1,7 +0,0 @@
-package birtix24
-
-type Contact struct {
-	Name       string `json:"NAME"`
-	SecondName string `json:"SECOND_NAME"`
-	LastName   string `json:"LAST_NAME"`
-}
